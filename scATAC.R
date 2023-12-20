b225d05431dff569ccd419ea13ce956c59968e42
Sherine Awad <sherinem@gl-login3.arc-ts.umich.edu>
2023-12-20 15:56:29 -0500

    Update scaTAC script

--- scATAC.R
+++ scATAC.R
@@ -58,48 +58,61 @@ myObject[["percent.mt"]] <- PercentageFeatureSet(myObject, pattern = "^mt-")
 
 ### Check cell quality
 figure_name <- ""
-figure_name <- paste(mysample, "qc1_vlnplot.pdf", sep="") 
+figure_name <- paste(mysample, "_QC_vlnplot.pdf", sep="") 
 pdf(file=figure_name)
-VlnPlot(object = myObject, features = c("nCount_RNA", "nFeature_RNA", "nCount_ATAC", "TSS.enrichment", "nucleosome_signal", "percent.mt"),ncol = 6)
+VlnPlot(object = myObject, features = c("nCount_RNA", "nFeature_RNA", "nCount_ATAC", "TSS.enrichment", "nucleosome_signal", "percent.mt"), size.title.use =0.1,  pt.size=0.1, ncol = 6)
 dev.off()
 
 myObject <- subset(x = myObject,subset = nCount_ATAC < 100000 & nCount_RNA < 30000 & nCount_ATAC > 1000 & nCount_RNA > 1000 & nFeature_RNA > 500 & nucleosome_signal < 1.2 & TSS.enrichment > 2 & percent.mt < 15)
 
 
-myObject$pct_reads_in_peaks <- myObject$peak_region_fragments / myObject$passed_filters * 100
-myObject$blacklist_ratio <- myObject$blacklist_region_fragments / myObject$peak_region_fragments
+#myObject$pct_reads_in_peaks <- myObject$peak_region_fragments / myObject$passed_filters * 100
+#myObject$blacklist_ratio <- myObject$blacklist_region_fragments / myObject$peak_region_fragments
 
 figure_name <- ""
-figure_name <- paste(mysample, "densityscatter.pdf", sep="") 
+figure_name <- paste(mysample, "_ATAC_DensityScatter.pdf", sep="") 
 pdf(file=figure_name)
-DensityScatter(myObject, x = 'nCount_peaks', y = 'TSS.enrichment', log_x = TRUE, quantiles = TRUE)
+DensityScatter(myObject, x = 'nCount_ATAC', y = 'TSS.enrichment', log_x = TRUE, quantiles = TRUE)
 dev.off()
 
 figure_name <- ""
-figure_name <- paste(mysample, "tssplot.pdf", sep="")
-pdf(file=figure_name)
-myObject$high.tss <- ifelse(myObject$TSS.enrichment > 3, 'High', 'Low')
-TSSPlot(myObject, group.by = 'high.tss') + NoLegend()
+figure_name <- paste(mysample, "_RNA_DensityScatter.pdf", sep="")
+pdf(file=figure_name) 
+DensityScatter(myObject, x ='nCount_RNA', y = 'TSS.enrichment', log_x = TRUE, quantiles = TRUE)
+dev.off() 
+
+figure_name <- "" 
+figure_name <- paste(mysample, "nucleosome_signalDensityScatter.pdf", sep="") 
+pdf(file=figure_name) 
+DensityScatter(myObject, x = 'nucleosome_signal' , y = 'TSS.enrichment', log_x = TRUE, quantiles = TRUE) 
+dev.off() 
+
+figure_name <- ""
+figure_name <- paste(mysample, "_vlpot.pdf", sep="")
+pdf(file="vlplot.pdf")
+VlnPlot(
+  object = myObject,
+  features = c('nCount_RNA', 'nFeature_RNA', 'nCount_ATAC', 'TSS.enrichment', 'nucleosome_signal'),
+  pt.size = 0.1,
+  ncol = 5, 
+  size.title.use = 0.1
+)
 dev.off()
 
 figure_name <- ""
-figure_name <- paste(mysample, "fragmentHist.pdf", sep="")
+figure_name <- paste(mysample, "_FragmentHist.pdf", sep="")
 pdf(file=figure_name)
 myObject$nucleosome_group <- ifelse(myObject$nucleosome_signal > 4, 'NS > 4', 'NS < 4')
 FragmentHistogram(object = myObject, group.by = 'nucleosome_group')
 dev.off()
 
-figure_name <- "" 
-figure_name <- paste(mysample, "vlpot.pdf", sep="")
-pdf(file="vlplot.pdf")
-VlnPlot(
-  object = myObject,
-  features = c('nCount_peaks', 'TSS.enrichment', 'blacklist_ratio', 'nucleosome_signal', 'pct_reads_in_peaks'),
-  pt.size = 0.1,
-  ncol = 5
-)
-dev.off() 
-
+figure_name <- ""
+figure_name <- paste(mysample, "_TSS.pdf", sep="")
+pdf(file=figure_name)
+myObject$high.tss <- ifelse(myObject$TSS.enrichment > 3, 'High', 'Low')
+table(myObject$high.tss)
+TSSPlot(myObject, group.by = 'High') + NoLegend()
+dev.off()
 
 saveRDS(myObject, file = myRDS)
 
